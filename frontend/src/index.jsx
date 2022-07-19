7714a01bc2c4811329badef22cb9489ce53ac46f
loicWCS <loic.lebon.wcs@gmail.com>
2022-07-19 11:28:54 +0200

    Context.provider dans index.js

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,27 +1,26 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable import/no-unresolved */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Bar from "./components/bar/bar";
 import Event from "./components/evenement/event";
 import Inscription from "./components/inscription/inscription";
 import Contact from "./components/contact/contact";
+import MyProvider from "./contexts/FormContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/event" element={<Event />} />
-        <Route path="/bar" element={<Bar />} />
-        <Route path="/" element={<App />} />
-        <Route path="/inscriptions/inscription" element={<Inscription />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route path="" element={} /> */}
-      </Routes>
-    </BrowserRouter>
+    <MyProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/inscription" element={<Inscription />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </MyProvider>
   </React.StrictMode>,
 );
